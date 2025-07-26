2970dd8bf95e064a813d46d4a05312b02a8f1c15
Fardin Ayar <fardin.aiar@gmail.com>
2025-07-26 13:39:14 +0330

    Add scripts to download checkpoints

---
+++ scripts/download_checkpoints.sh
@@ -0,0 +1,11 @@
+# ImVoxelNet for Image-based 3D Object Detection
+gdown https://drive.google.com/file/d/1nrUjGVD-DHEBYhCOrRrIT37hsqgHhWRF/view?usp=share_link --fuzzy -O checkpoints/ImVoxelNet.pth
+
+# MVXNet for Fusion-based 3D Object Detection
+gdown https://drive.google.com/file/d/1dtTEuCzsj1I69vz6Hy2I6KZb515R-zoZ/view?usp=sharing --fuzzy -O checkpoints/MVXNet.pth
+
+# Second for LiDAR-based 3D Object Detection
+gdown https://drive.google.com/file/d/1kqIqwebq51OeAl15NkQppTSmDrzMv9F9/view?usp=sharing --fuzzy -O checkpoints/Second.pth
+
+# PointPillars for LiDAR-based 3D Object Detection
+gdown https://drive.google.com/file/d/1CeYLmk5CVyxkRakshjKdsDJcqxstdS5T/view?usp=sharing --fuzzy -O checkpoints/PointPillars.pth
