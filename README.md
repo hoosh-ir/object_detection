0a9040d835f59ed945aa0035de75df5bbd343631
Fardin Ayar <fardin.aiar@gmail.com>
2025-07-30 18:08:12 +0330

    Update readme

--- README.md
+++ README.md
@@ -48,19 +48,50 @@ The following code is also provided in `infer_example.py`
 ```python
 from online_inference_plugin.inference_api import InferenceLidarAPI
 from online_inference_plugin.data import load_pcd
+import numpy as np
+
+def rotate_shift_lidar(lidar, pitch_angle=5.0, shift_z_up=2.5):
+    # Rotate lidar around y axis and shift z up
+        
+    pitch_angle = pitch_angle * np.pi / 180.0  # Convert 5 degrees to radians
+    cos_pitch = np.cos(pitch_angle)
+    sin_pitch = np.sin(pitch_angle)
+
+    rotation_matrix = np.array([
+        [cos_pitch,  0, sin_pitch],
+        [0,          1, 0        ],
+        [-sin_pitch, 0, cos_pitch]
+    ])
+
+    xyz_rotated = np.dot(lidar[:, :3], rotation_matrix.T)
+    lidar[:, :3] = xyz_rotated
+
+    lidar[:,2] += shift_z_up
+    return lidar
+
 
 # Create inference API. It can be slow, so we recommend to create it once and reuse it.
 inference_api = InferenceLidarAPI("pointpillars")
 
-# Load lidar data with any tool you want. It just needs to be a N*4 numpy array.
-lidar = load_pcd("online_inference_plugin/example_data_dairv2x_i/velodyne/000009.pcd")
-# lidar = np.load("example-cooperative-vehicle-infrastructure/infrastructure-side/velodyne/000009.npy")
+# Load lidar data with any tool you want. It just needs to be a 4xN numpy array.
+#lidar = load_pcd("online_inference_plugin/example_data_dairv2x_i/velodyne/000009.pcd")
+lidar = np.load("_lidar3/_lidar3_1752394146165657614_000000.npy").astype(np.float32)
 # lidar = ...
 
+# The dairv2x assume lidar pitch is 0, and its height is around 2.5m - 3m. 
+# Our lidar on the other hand is tilted 5 degrees toward ground, and its height is around 5.
+# So we need to rotate and shift the lidar to make it compatible with the dairv2x.
+lidar = rotate_shift_lidar(lidar)
+
+# Add column of ones to the lidar data
+lidar = np.concatenate([lidar, np.zeros((lidar.shape[0], 1))+0], axis=1)
+
+
 # Run inference. For now, we only support lidar data.
-# If you want to visualize the result, set show=True.
-result = inference_api(lidar, show=True)
+result, model_data = inference_api(lidar)
 print(result)
+# Visualize the result
+inference_api.visualize(model_data, result, score_thr=0.8)
 ```
 
 #### Available Models
--- single_point_infer_example.py
+++ single_point_infer_example.py
@@ -3,38 +3,36 @@ from online_inference_plugin.data import load_pcd
 import numpy as np
 
 def rotate_shift_lidar(lidar, pitch_angle=5.0, shift_z_up=2.5):
+    # Rotate lidar around y axis and shift z up
         
-    # Correct for 5-degree pitch toward ground
     pitch_angle = pitch_angle * np.pi / 180.0  # Convert 5 degrees to radians
     cos_pitch = np.cos(pitch_angle)
     sin_pitch = np.sin(pitch_angle)
 
-    # Rotation matrix around Y-axis to correct pitch
-    # R_y = [cos(θ)   0   sin(θ)]
-    #       [0        1   0     ]
-    #       [-sin(θ)  0   cos(θ)]
     rotation_matrix = np.array([
         [cos_pitch,  0, sin_pitch],
         [0,          1, 0        ],
         [-sin_pitch, 0, cos_pitch]
     ])
 
-    # Apply rotation to X, Y, Z coordinates
     xyz_rotated = np.dot(lidar[:, :3], rotation_matrix.T)
     lidar[:, :3] = xyz_rotated
 
-    ## Substract 2 from z of points
     lidar[:,2] += shift_z_up
     return lidar
 
 
 # Create inference API. It can be slow, so we recommend to create it once and reuse it.
-inference_api = InferenceLidarAPI("second")
+inference_api = InferenceLidarAPI("pointpillars")
 
 # Load lidar data with any tool you want. It just needs to be a 4xN numpy array.
 #lidar = load_pcd("online_inference_plugin/example_data_dairv2x_i/velodyne/000009.pcd")
-lidar = np.load("_lidar3/_lidar3_1752394150421749713_000017.npy").astype(np.float32)
+lidar = np.load("_lidar3/_lidar3_1752394146165657614_000000.npy").astype(np.float32)
 # lidar = ...
+
+# The dairv2x assume lidar pitch is 0, and its height is around 2.5m - 3m. 
+# Our lidar on the other hand is tilted 5 degrees toward ground, and its height is around 5.
+# So we need to rotate and shift the lidar to make it compatible with the dairv2x.
 lidar = rotate_shift_lidar(lidar)
 
 # Add column of ones to the lidar data
@@ -42,8 +40,7 @@ lidar = np.concatenate([lidar, np.zeros((lidar.shape[0], 1))+0], axis=1)
 
 
 # Run inference. For now, we only support lidar data.
-# If you want to visualize the result, set show=True.
 result, model_data = inference_api(lidar)
 print(result)
-
-inference_api.visualize(model_data, result, score_thr=0.7)
+# Visualize the result
+inference_api.visualize(model_data, result, score_thr=0.8)
